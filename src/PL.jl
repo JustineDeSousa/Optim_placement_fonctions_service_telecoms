5c09c1a8b259b97c70326c90b827a212dc792ed6
JustineDeSousa <justine.desousa6@gmail.com>
2022-02-15 08:34:39 +0100

    Update PL.jl

--- src/PL.jl
+++ src/PL.jl
@@ -24,19 +24,32 @@ end
 function prec(node::Int)
 	return [inst.graph[arc,1] for arc in 1:nb_arcs if inst.graph[arc,2] == node]
 end
+# conserv_s = @constraint(m, [k in 1:K], base_name="conserv_s",
+	# sum( sum( x[s[k],j,k,c] for j in succ(s[k])) - sum( x[j, s[k],k,c] for j in prec(s[k]) ) - x[s[k],s[k],k,c] for c in 1:C[k]) == 1)
+# conserv_t = @constraint(m, [k in 1:K], base_name="conserv_t",
+	# sum( sum( x[t[k],j,k,c] for j in succ(s[k])) - sum( x[j, t[k],k,c] for j in prec(s[k]) ) - x[t[k],t[k],k,c] for c in 1:C[k]) == -1)
 conserv_s = @constraint(m, [k in 1:K], base_name="conserv_s",
-	sum( sum( x[s[k],j,k,c] for j in succ(s[k])) - sum( x[j, s[k],k,c] for j in prec(s[k]) ) - x[s[k],s[k],k,c] for c in 1:C[k]) == 1)
+	sum( x[s[k],j,k,1] for j in succ(s[k])) - sum( x[j, s[k],k,1] for j in prec(s[k]) ) - x[s[k],s[k],k,1] == 1)
 conserv_t = @constraint(m, [k in 1:K], base_name="conserv_t",
-	sum( sum( x[t[k],j,k,c] for j in succ(s[k])) - sum( x[j, t[k],k,c] for j in prec(s[k]) ) - x[t[k],t[k],k,c] for c in 1:C[k]) == -1)
-conserv = @constraint(m, [k in 1:K, i=1:n; i!=s[k] && i!=t[k]], base_name="Conserv_k_i",
-	sum( sum( x[i,j,k,c] for j in succ(i)) - sum( x[j, i,k,c] for j in prec(i) ) for c in 1:C[k]) == 0)
+	sum( x[t[k],j,k,C[k]] for j in succ(s[k])) - sum( x[j, t[k],k,C[k]] for j in prec(s[k]) ) - x[t[k],t[k],k,C[k]] == -1)
+
+# conserv = @constraint(m, [k in 1:K, i=1:n; i!=s[k] && i!=t[k]], base_name="Conserv_k_i",
+	# sum( sum( x[i,j,k,c] for j in succ(i)) - sum( x[j, i,k,c] for j in prec(i) ) for c in 1:C[k]) == 0)
+conserv = @constraint(m, [i=1:n, k in 1:K, c in 1:C[k]; i!=s[k] && i!=t[k]], base_name="Conserv_k_i",
+	sum( x[i,j,k,c] for j in succ(i)) - sum( x[j, i,k,c] for j in prec(i) ) == 0)
+
 
 #Contraintes du pb : capacité, latence
 latence = @constraint(m, [k in 1:K], base_name="latence_max",
 	sum( sum( inst.graph[arc,5]*x[inst.graph[arc,1],inst.graph[arc,2],k,c] for arc in 1:nb_arcs) for c in 1:C[k]) <= inst.commod[k,4] )
+# capa_function = @constraint(m, [f in 1:F, i in 1:n], base_name="capa_funct_f_i",
+	# sum( sum( x[i,i,k,c] for c in 1:C[k])*inst.commod[k,3] for k in 1:K) <= inst.functions[f,1]*y[f,i])
+function couche_of(k::Int, f::Int)
+	index = findfirst(x->x==f, inst.orders[k,f])
+	return index
+end
 capa_function = @constraint(m, [f in 1:F, i in 1:n], base_name="capa_funct_f_i",
-	sum( sum( x[i,i,k,c] for c in 1:C[k])*inst.commod[k,3] for k in 1:K) <= inst.functions[f,1]*y[f,i])
-
+	sum( x[i,i,k,inst.orders[k,couche_of(k,f)]] *inst.commod[k,3] for k in 1:K if couche_of(k,f) != nothing) <= inst.functions[f,1]*y[f,i])
 function capa_N(node::Int)
 	index = findfirst(x->x==node, inst.graph[:,1:2])
 	return inst.graph[index[1], index[2]+2]
@@ -50,6 +63,9 @@ end
 #Ordre d'utilisation des fonctions
 exclusion = @constraint(m, [i in 1:n, k in 1:K], base_name="exclusion_i_k_", x[i,i,k,inst.affinity[k,1]] <= 1 - x[i,i,k,inst.affinity[k,2]] )
 couche = @constraint(m, [k in 1:K, c in 1:C[k]], base_name="couche_k_c", sum(x[i,i,k,c] for i in 1:n) == 1 )
+order = @constraint(m, [i in 1:n, k in 1:K, c in 1:C[k]], base_name="orders_i", x[i,i,k,c] <= inst.orders[k,c])
+ouverture_noeud = @constraint(m, [i in 1:n, k in 1:K, c in 1:C[k]], base_name="ouverture_noeud_i_k_c",
+	x[i,i,k,c] <= u[i])
 
 #Objectif
 function node_cost(node::Int)
@@ -61,17 +77,19 @@ end
 optimize!(m)
 if termination_status(m) == OPTIMAL
     println("Solution is optimal")
+	for k in 1:K
+		println("Client ", k, " : ")
+		for c in 1:C[k]
+			print("\tCouche ", c, " -> ")
+			solution = [(i,j) for i in 1:n, j in 1:n if value(x[i,j,k,c]) > 0 ]
+			println(solution)
+		end
+	end
+	println(value.(y))
 elseif termination_status(m) == TIME_LIMIT && has_values(m)
     println("Solution is suboptimal due to a time limit, but a primal solution is available")
 else
     error("The model was not solved correctly.")
 end
 println("  objective value = ", objective_value(m))
-for k in 1:K
-	println("Client ", k, " : ")
-	for c in 1:C[k]
-		print("\tCouche ", c, " -> ")
-		solution = [(i,j) for i in 1:n, j in 1:n if value(x[i,j,k,c]) > 0 ]
-		println(solution)
-	end
-end
+

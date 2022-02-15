c2481b7f5336d7b58bec8f2fe087f0be95f085cc
Yue0925 <mellet1997@outlook.com>
2022-02-15 16:52:48 +0100

    tmp

--- src/run.jl
+++ src/run.jl
@@ -1,5 +1,7 @@
 using CPLEX 
 using JuMP
+
+
 include("mip.jl")
 
 function test1()
@@ -13,8 +15,11 @@ function test1()
             for subfile in readdir(path)
                 path = dir * file * subfile
                 println("path : ", path)
-
-                
+                ss = split(subfile, "_")
+                name = String(ss[1])
+                num = parse(Int64, ss[2])
+                println("name : ", name, ", num : ", num)
+                global data = Data(dir, false, name, num) # reading function works
             end
         end
     end
@@ -23,11 +28,11 @@ end
 
 
 function test()
-    # dir = "../small_data/"
-    # global data = Data(dir, true, "grille2x3")
+    dir = "../small_data/"
+    global data = Data(dir, true, "test")
 
-    dir = "../data/"
-    global data = Data(dir, false, "pdh", 1)
+    # dir = "../data/"
+    # global data = Data(dir, false, "pdh", 1)
 
     cplexSolveMIP(data)
 end

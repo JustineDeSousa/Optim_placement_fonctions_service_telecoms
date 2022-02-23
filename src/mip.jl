1347884e84f7cbaf167123a32246c9f065abea41
Yue0925 <mellet1997@outlook.com>
2022-02-23 18:18:48 +0100

    Update mip.jl

--- src/mip.jl
+++ src/mip.jl
@@ -50,8 +50,8 @@ function cplexSolveMIP(data::Data)
     # flux constraints
     # ------------------
     for k in 1:data.K
-        s = round(Int, data.Commodidty[k, 1])
-        t = round(Int, data.Commodidty[k, 2])
+        s = round(Int, data.Commodity[k, 1])
+        t = round(Int, data.Commodity[k, 2])
 
         # conversation flux at s
         @constraint(M, sum(sum(x[s, j, k, c] for j in 1:data.N if data.Adjacent[s, j]) - 
@@ -71,7 +71,7 @@ function cplexSolveMIP(data::Data)
         end
 
         # -----------------------------------------
-        # flux conversation at each layer
+        # flux conservation at each layer
         # -----------------------------------------
 
         for i in 1:data.N
@@ -112,7 +112,7 @@ function cplexSolveMIP(data::Data)
 
     # constraint maximal latency 
     @constraint(M, [k in 1:data.K], sum( sum(data.LatencyMat[a, 3] * x[round(Int, data.LatencyMat[a, 1]), round(Int, data.LatencyMat[a, 2]), k, c]
-    for a in 1:data.M) for c in 1:data.Layer[k] ) <= data.Commodidty[k, 4])
+    for a in 1:data.M) for c in 1:data.Layer[k] ) <= data.Commodity[k, 4])
     
 
 
@@ -133,10 +133,10 @@ function cplexSolveMIP(data::Data)
 
     # constraint function capacitiy
     @constraint(M, [i in 1:data.N, f in 1:data.F], sum( sum(x[i, i, k, c] for c in 1:data.Layer[k] if c==lay(k,f,data))
-        * round(Int, data.Commodidty[k, 3]) for k in 1:data.K) <= data.CapacityFun[f] * y[f, i])
+        * round(Int, data.Commodity[k, 3]) for k in 1:data.K) <= data.CapacityFun[f] * y[f, i])
     
     # constraint machine capacity
-    @constraint(M, [i in 1:data.N], sum(y[f, i] for f in 1:data.F) <= data.CapacityNode[i])
+    # @constraint(M, [i in 1:data.N], sum(y[f, i] for f in 1:data.F) <= data.CapacityNode[i])
 
 
     # constraint of variable u
@@ -168,15 +168,15 @@ function cplexSolveMIP(data::Data)
     end
     
     # for each layer, at least one function is installed
-    @constraint(M, [k in 1:data.K, c in 1:data.Layer[k]], sum(x[i, i, k, c] for i in 1:data.N) >= 1)
-
+    @constraint(M, [k in 1:data.K, c in 1:data.Layer[k]], sum(x[i, i, k, c] for i in 1:data.N) == 1)
 
 
     # solve the problem
     optimize!(M)
-    # println(solution_summary(M))
+    # TODO : I'm not sure if this summary works for you
+    println(solution_summary(M))
 
-    exploredNodes = MOI.get(backend(M), MOI.NodeCount())
+    #exploredNodes = MOI.get(backend(M), MOI.NodeCount())
     
     solveTime = MOI.get(M, MOI.SolveTime())
 
@@ -188,17 +188,19 @@ function cplexSolveMIP(data::Data)
     println("isOptimal ? ", isOptimal)
     println("solveTime = ", solveTime)
 
-    # 
     compute_conflict!(M)
 
     if has_values(M)
         GAP = MOI.get(M, MOI.RelativeGap())
         obj_val = objective_value(M)
-        best_bound = objective_bound(M)
+        # best_bound = objective_bound(M)
 
         println("obj_val = ", obj_val)
         # println("best_bound = ", best_bound)
         # println("GAP = ", GAP)
+
+        commodities_path = [[] for _ in 1:data.K] # Array{Array{Tuple{Int64,Int64},1},1}()
+        fun_placement = zeros(Int64, data.F, data.N)
 
         for k in 1:data.K
             println("Client ", k, " : ")
@@ -207,8 +209,18 @@ function cplexSolveMIP(data::Data)
                 solution = [(i,j) for i in 1:data.N, j in 1:data.N if value(x[i,j,k,c]) > TOL ]
                 println(solution)
             end
+
+            commodities_path[k] = [(i, j) for c in 1:data.Layer[k], i in 1:data.N, j in 1:data.N if i != j && value(x[i,j,k,c]) > TOL]
+        end
+        for i in 1:data.F, j in 1:data.N
+            if JuMP.value(y[i, j]) > TOL
+                fun_placement[i, j] = round(Int, JuMP.value(y[i, j]))
+            end
         end
         println("y = ", value.(y))
+        
+        # check feasibility
+        verificationMIP(data, commodities_path, fun_placement)
 
     elseif MOI.get(M, MOI.ConflictStatus()) != MOI.CONFLICT_FOUND
         conflict_constraint_list = ConstraintRef[]
@@ -224,5 +236,64 @@ function cplexSolveMIP(data::Data)
         error("No conflict could be found for an infeasible model.")
     end
 
+end
 
+
+
+function verificationMIP(data::Data, commodities_path::Array{Array{Any,1},1}, fun_placement::Array{Int64,2})
+    # for each commodity, a valid path from s to t
+    println("commodities_path : ", commodities_path)
+    println("fun_placement : ", fun_placement)
+
+    if isConnectedComponent(commodities_path, data) == false
+        return false
+    end
+
+    # for i in 1:data.N
+        
+    # end
+
+end
+
+
+function isConnectedComponent(commodities_path::Array{Array{Any,1},1}, data::Data)
+    # for each Commodity
+    for k in 1:data.K
+        v = Set{Int64}()
+        sort!(commodities_path[k])
+        for (i, j) in commodities_path[k]
+            push!(v, i)
+            push!(v, j)
+        end
+        vertices = collect(v)
+        println("vertices : ", vertices)
+
+
+        isVisited = Dict(i=>false for i in vertices)
+        todo = []
+    
+        # pick up a source
+        s = round(Int, data.Commodity[k, 1])
+        append!(todo, s)
+
+        while size(todo, 1) >0
+            v = pop!(todo)
+            if ! isVisited[v] 
+                isVisited[v] = true
+                for u in vertices
+                    if data.Adjacent[v, u] && ! isVisited[u]
+                        append!(todo, u)
+                    end
+                end
+            end
+        end
+
+        for v in vertices
+            if ! isVisited[v]
+                return false
+            end
+        end
+
+    end
+    return true
 end

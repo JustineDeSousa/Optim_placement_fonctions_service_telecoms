fb209babff91b8d789011a1ba6d9402af7f3b756
JustineDeSousa <justine.desousa6@gmail.com>
2022-02-25 11:20:49 +0100

    find_path(data,k) ok

--- src/run.jl
+++ src/run.jl
@@ -1,12 +1,15 @@
 include("mip.jl")
+include("heuristic.jl")
 
 
 function test()
-    dir = "../small_data/"
-    global data = Data(dir, true, "grille2x3")
-    dir = "../data/"
-    #global data = Data(dir, false, "pdh", 1)
+    global data = Data("test", 1, true)
+    for k in 1:data.K
+        println("path commodity ", k, "  : ", find_path(data, k))
+    end
+    
+    #global data = Data("pdh", 1)
 
-    cplexSolveMIP(data)
+    # cplexSolveMIP(data)
 end
 

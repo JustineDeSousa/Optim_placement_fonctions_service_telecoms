e4c642319cfe0f69eb747601d0d8b6679c20c09c
JustineDeSousa <justine.desousa6@gmail.com>
2022-03-31 10:45:48 +0200

    correction affichage

--- src/run.jl
+++ src/run.jl
@@ -65,7 +65,7 @@ function solve_instances(method::String; maxTime::Float64=10.0)
             if !isfile(outputFile) #if the instance hasn't been solved already
                 data = Data(instanceName, num)
                 if method == "MIP"
-                    @info "LP : " * instanceName * "_" * string(num)
+                    @info "MIP : " * instanceName * "_" * string(num)
                     @time (paths, cost, resolution_time) = cplexSolveMIP(data)
                     ite = 0
                     functions = []
